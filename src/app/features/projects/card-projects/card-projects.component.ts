eb8e4a50a544cfdf60d0fb028be8349e861f6f4c
david <deivid.develop@gmail>
2020-09-18 10:55:39 +0200

    Update animation cards-projects

--- src/app/features/projects/card-projects/card-projects.component.ts
+++ src/app/features/projects/card-projects/card-projects.component.ts
@@ -17,16 +17,16 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
         query(':enter', style({ opacity: 0 }), { optional: true }),
 
         // Each card will appear sequentially with the delay of 300ms
-        query(':enter', stagger('300ms',
+        query(':enter', stagger('400ms',
         [
         animate('.5s ease-in', keyframes(
         [
-            style({ opacity: 0, transform: 'translateY( 50%)', offset: 0 }),
-            style({ opacity: .5, transform: 'translateY( 10px) scale(1.1)', offset: 0.3 }),
+            style({ opacity: 0.2, transform: 'translateY( 50%)', offset: 0 }),
+            style({ opacity: .6, transform: 'translateY( 10px) scale(1.1)', offset: 0.3 }),
             style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
           ]))
           ]), { optional: true }),
-        query(':leave', stagger('200ms',
+        /*query(':leave', stagger('200ms',
         [
         animate('.5s ease-in', keyframes(
           [
@@ -34,7 +34,7 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
             style({ opacity: .5, transform: 'translateY(10px) scale(1.1)', offset: 0.3 }),
             style({ opacity: 1, transform: 'translateY(50%)', offset: 1 })
           ]))
-        ]), { optional: true })
+        ]), { optional: true })*/
       ]),
     ]),
 
