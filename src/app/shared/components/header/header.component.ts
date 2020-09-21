bf8b9e97316c132e7a025ece2320844c6c13d106
david <deivid.develop@gmail>
2020-09-21 09:32:39 +0200

    Added id at menu-items

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -10,16 +10,19 @@ export class HeaderComponent implements OnInit {
 
   menuItems: MenuItem[] = [
     {
+      id: 1,
       label: 'About',
       icon: 'assignment_ind',
       route: 'about'
     },
     {
+      id: 2,
       label: 'Works',
       icon: 'ad_units',
       route: 'projects'
     },
     {
+      id: 3,
       label: 'Blog',
       icon: 'article',
       route: 'blog'
