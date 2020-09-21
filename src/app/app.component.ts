92362908d5e104f5ce87e0d4594e4b7177d8143b
david <deivid.develop@gmail>
2020-09-21 09:28:29 +0200

    Update animation routerTransition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,12 @@ import {trigger, animate, style, group, animateChild, query, stagger, transition
         query(':enter, :leave', style({position: 'fixed', width:'100%' }), { optional: true }),
         group([
           query(':enter', [
-            style({ transform: 'translateX(-100%)' }),
-            animate('0.5s ease-in-out', style({ opacity: 1, transform: 'translateX(0%)' }))
+            style({ transform: 'translateY(-18%)' }),
+            animate('0.8s ease-in-out', style({ opacity: 1, transform: 'translateY(0%)' }))
           ], { optional: true }),
           query(':leave', [
-            style({ transform: 'translateX(100%)' }),
-            animate('0.1s ease-in-out', style({ opacity: 0, transform: 'translateX(100%)' }))
+            style({ transform: 'translateY(100%)' }),
+            animate('0.1s ease-in-out', style({ opacity: 0.8, transform: 'translateY(100%)' }))
           ], { optional: true }),
         ])
       ])
