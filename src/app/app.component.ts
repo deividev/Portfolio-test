bcc84f25d4cc102d359e53a233b1c8a640c0b846
david <deivid.develop@gmail>
2020-09-21 13:37:45 +0200

    All texts changed to Spanish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,7 +46,6 @@ export class AppComponent {
   {
     tsParticles.loadJSON("tsparticles", "assets/particles.json")
     .then((container) => {
-      console.log("callback - tsparticles config loaded");
     })
     .catch((error) => {
       console.error(error);
