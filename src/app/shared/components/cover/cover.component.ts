24b1058c06fbcf88ca7e14d8436f95c23b052fdb
david <deivid.develop@gmail>
2020-09-23 20:04:40 +0200

    Added icon var

--- src/app/shared/components/cover/cover.component.ts
+++ src/app/shared/components/cover/cover.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faHtml5, faCss3Alt, faJs, faAngular, faVuejs, faBootstrap, faNode, faDigitalOcean, faSass, faGitAlt } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-cover',
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cover.component.scss']
 })
 export class CoverComponent implements OnInit {
+  faHtml5 = faHtml5;
+  faCss3Alt = faCss3Alt;
+  faSass = faSass
+  faJs = faJs
+  faAngular = faAngular
+  faVue = faVuejs
+  faBootstrap = faBootstrap
+  faNode = faNode
+  faDigitalOcean = faDigitalOcean
+
+  faGit = faGitAlt
+
   isActive = true;
   constructor() { }
 
