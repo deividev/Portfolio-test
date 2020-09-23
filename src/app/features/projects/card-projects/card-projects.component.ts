bed8666ec62907639c2f99c7d2afa8e2c4af3287
david <deivid.develop@gmail>
2020-09-23 10:11:16 +0200

    Update animation card-projects

--- src/app/features/projects/card-projects/card-projects.component.ts
+++ src/app/features/projects/card-projects/card-projects.component.ts
@@ -17,9 +17,9 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
         query(':enter', style({ opacity: 0 }), { optional: true }),
 
         // Each card will appear sequentially with the delay of 300ms
-        query(':enter', stagger('400ms',
+        query(':enter', stagger('300ms',
         [
-        animate('.8s ease-in', keyframes(
+        animate('.6s ease-in', keyframes(
         [
             style({ opacity: 0.2, transform: 'translateY( 50%)', offset: 0 }),
             style({ opacity: .8, transform: 'translateY( 10px) scale(1.1)', offset: 0.3 }),
